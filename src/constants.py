d99f834453570e4ee480abe7dd6d32888f33e552
Mina <minasent@web.de>
2021-07-20 10:42:55 +0200

    branch update ui 4 OOP refactored

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+BG_COL = "#EBF2F5"
+FONT = "Arial"
+
+FG_LBL_COL = "#0D1F2D"
+FG_BTN_COL = "#0D1F2D"
+BG_BTN_COL = "#9EA3B0"
+HL_COL = "#9EA3B0"
+
+FT_LBL_TITLE = (FONT, 24, "bold")
+FT_LBL_DESCR = (FONT, 16, "normal")
+FT_LBL_NORM = (FONT, 11, "normal")
+
+FT_BTN_NORM = (FONT, 10, "normal")
+
+WD_TEXTBOXES = 55
